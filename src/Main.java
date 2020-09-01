700731ff42410f71d9f150b8f2884093955324a6
siddhant <siddhantsangwan@yahoo.com>
2020-09-01 21:51:45 +0530

    add thread creation

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("We are now in thread " + Thread.currentThread().getName());
+                System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
+                throw new RuntimeException("Intentional Exception");
+            }
+        });
+
+        // set the name of the new thread
+        thread.setName("New Worker Thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
+
+        // need to actually run the thread
+        // notice static methods
+        System.out.println("We are in thread " + Thread.currentThread().getName() + " before starting a new thread");
+        thread.start();
+        System.out.println("We are in thread " + Thread.currentThread().getName() + " after starting a new thread");
+
+        // usually an error will bring down a thread
+        // but we can catch it
+        // the exception handler will get it if nothing else has
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error happened in thread " + t.getName() + " and the error" +
+                        " message is " + e.getMessage());
+            }
+        });
+
+        // make the main thread sleep for 10000 milliseconds
+        // this isn't spinning or busy waiting, actually the os has been instructed not to schedule the thread
+        // for the specified time
+        Thread.sleep(10000);
+
+
+    }
+}
