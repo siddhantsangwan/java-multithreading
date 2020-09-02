68e6f0579f06151879f1e36b3fcb50196ac8611e
siddhant <siddhantsangwan@yahoo.com>
2020-09-02 22:04:09 +0530

    add second technique of thread creation

--- src/Main.java
+++ src/Main.java
@@ -20,9 +20,11 @@ public class Main {
         thread.start();
         System.out.println("We are in thread " + Thread.currentThread().getName() + " after starting a new thread");
 
-        // usually an error will bring down a thread
-        // but we can catch it
-        // the exception handler will get it if nothing else has
+        /*
+         usually an error will bring down a thread
+         but we can catch it
+         the exception handler will get it if nothing else has
+        */
         thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
             @Override
             public void uncaughtException(Thread t, Throwable e) {
@@ -31,11 +33,26 @@ public class Main {
             }
         });
 
-        // make the main thread sleep for 10000 milliseconds
-        // this isn't spinning or busy waiting, actually the os has been instructed not to schedule the thread
-        // for the specified time
+        /*
+         make the main thread sleep for 10000 milliseconds
+         this isn't spinning or busy waiting, actually the os has been instructed not to schedule the thread
+         for the specified time
+        */
         Thread.sleep(10000);
 
+        Thread thread1 = new NewThread();
+        thread1.start();
+    }
 
+    /*
+     this is the other way, directly extend the Thread class
+     which implements Runnable (and hence the run() method)
+    */
+    private static class NewThread extends Thread {
+        @Override
+        public void run() {
+            // Code that executes on the new thread
+            System.out.println("Hello from " + Thread.currentThread().getName());
+        }
     }
 }
