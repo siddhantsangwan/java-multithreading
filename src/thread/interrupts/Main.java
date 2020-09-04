682f7d765f64f164fc31475e45d7f74bbc01a4c3
siddhant <siddhantsangwan@yahoo.com>
2020-09-04 17:04:05 +0530

    update interrupts file

--- src/thread/interrupts/Main.java
+++ src/thread/interrupts/Main.java
@@ -1,2 +1,28 @@
-package thread.interrupts;public class Main {
+package thread.interrupts;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new BlockingClass());
+        thread.start();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // interrupt the BlockingClass thread that keeps running even after the main thread ends
+        thread.interrupt();
+    }
+
+    // sleeps for a long time
+    private static class BlockingClass implements Runnable {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(500000);
+            } catch (InterruptedException e) {
+                // catch the interrupt
+                System.out.println("BlockingThread interrupted");
+            }
+        }
+    }
 }
